11b1aa43268e771078e76769e10f0f9f5aa8bc52
Sufyan Shaik <mdsufyan7@gmail.com>
2024-02-11 05:51:18 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVDHEGhWyTSlInM1j8amla1EPq8356BX4",
+  authDomain: "muslimawaaz-432c1.firebaseapp.com",
+  databaseURL: "https://muslimawaaz-432c1.firebaseio.com",
+  projectId: "muslimawaaz-432c1",
+  storageBucket: "muslimawaaz-432c1.appspot.com",
+  messagingSenderId: "642204247107",
+  appId: "1:642204247107:web:4a2bde5529880182fbe0f6",
+  measurementId: "G-6T47JPB0TE"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, app }
