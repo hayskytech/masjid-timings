e3f09027767d6b61236d44cffe669e1ca64cf226
Abdulla <smdabdulla510@gmail.com>
2024-02-15 17:30:03 +0530

    changes with css

--- src/comp/MasjidTimes.js
+++ src/comp/MasjidTimes.js
@@ -1,65 +1,55 @@
-import React, { useContext, useState } from 'react'
-import { useEffect } from 'react'
-import { onValue, push, ref, set } from 'firebase/database'
-import { Button, Form, Icon, List, Modal } from 'semantic-ui-react'
-import { MyContext } from '../App'
-import { db } from '../firebase'
+import React, { useContext, useState, useEffect } from 'react';
+import { onValue, ref } from 'firebase/database';
+import { List, Icon } from 'semantic-ui-react';
+import { MyContext } from '../App';
+import { db } from '../firebase';
 
 export default function MasjidTimes() {
-  const { user } = useContext(MyContext)
-  const [box, setBox] = useState(false)
-  const [Name, setName] = useState('')
-  const [Fajr, setFajr] = useState('')
-  const [Zohar, setZohar] = useState('')
-  const [Asar, setAsar] = useState('')
-  const [Magrib, setMagrib] = useState('')
-  const [Juma, setJuma] = useState('')
-  const [Isha, setIsha] = useState('')
-  const [list, setList] = useState([])
-  const [editid, seteditid] = useState(null)
+  const { user } = useContext(MyContext);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    const myRef = ref(db, 'timings/masjid/')
+    const myRef = ref(db, 'timings/masjid/');
     onValue(myRef, (snapshot) => {
-      const res = snapshot.val()
-      setList(res);
-    })
-  }, [])
+      const res = snapshot.val();
+      setList(res || []);
+    });
+  }, []);
+
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h3>Namaz Timings</h3>
-      <table>
-        <tbody>
-          <tr>
-            <td>Name:</td>
-            <td>{Name}</td>
-          </tr>
-          <tr>
-            <td>Fajr:</td>
-            <td>{Fajr}</td>
-          </tr>
-          <tr>
-            <td>Zohar:</td>
-            <td>{Zohar}</td>
-          </tr>
-          <tr>
-            <td>Asar:</td>
-            <td>{Asar}</td>
-          </tr>
-          <tr>
-            <td>Magrib:</td>
-            <td>{Magrib}</td>
-          </tr>
-          <tr>
-            <td>Isha:</td>
-            <td>{Isha}</td>
-          </tr>
-          <tr>
-            <td>Juma:</td>
-            <td>{Juma}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div style={{ margin: 'auto', display: 'inline-block' }}>
+        <table style={{ borderCollapse: 'collapse', border: '1px solid black' }}>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Name:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Name</th>
+            </tr>
+            <tr> <th style={{ border: '1px solid black', padding: '8px' }}>Fajr:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+            <tr><th style={{ border: '1px solid black', padding: '8px' }}>Zohar:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+            <tr> <th style={{ border: '1px solid black', padding: '8px' }}>Asar:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+            <tr>  <th style={{ border: '1px solid black', padding: '8px' }}>Magrib:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+            <tr> <th style={{ border: '1px solid black', padding: '8px' }}>Isha:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+            <tr>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Juma:</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
