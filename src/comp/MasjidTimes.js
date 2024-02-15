9c173c76635080194b3a974fee4ddc86f66c3777
Abdulla <smdabdulla510@gmail.com>
2024-02-15 17:11:26 +0530

    changes with css

--- src/comp/MasjidTimes.js
+++ src/comp/MasjidTimes.js
@@ -25,66 +25,41 @@ export default function MasjidTimes() {
       setList(res);
     })
   }, [])
-
-  function editItem(key) {
-    seteditid(key)
-    setName(list[key].Name)
-    setFajr(list[key].Fajr)
-    setZohar(list[key].Zohar)
-    setAsar(list[key].Asar)
-    setMagrib(list[key].Magrib)
-    setJuma(list[key].Juma)
-    setIsha(list[key].Isha)
-  }
-
-  function saveItem() {
-    if (Name && Fajr && Zohar && Asar && Magrib && Juma && Isha) {
-      set(ref(db, 'timings/masjid/' + editid), { Name, Fajr, Zohar, Asar, Magrib, Juma, Isha })
-      seteditid(null)
-      setBox(false)
-    } else {
-      window.alert('Please enter all the values')
-    }
-  }
-
   return (
     <div>
-
-      <Form onSubmit={editid ? saveItem : null} id='mainForm'>
-        <h3>Namaz Timings</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>Name:</td>
-              <td>{Name}</td>
-            </tr>
-            <tr>
-              <td>Fajr:</td>
-              <td>{Fajr}</td>
-            </tr>
-            <tr>
-              <td>Zohar:</td>
-              <td>{Zohar}</td>
-            </tr>
-            <tr>
-              <td>Asar:</td>
-              <td>{Asar}</td>
-            </tr>
-            <tr>
-              <td>Magrib:</td>
-              <td>{Magrib}</td>
-            </tr>
-            <tr>
-              <td>Isha:</td>
-              <td>{Isha}</td>
-            </tr>
-            <tr>
-              <td>Juma:</td>
-              <td>{Juma}</td>
-            </tr>
-          </tbody>
-        </table>
-      </Form>
+      <h3>Namaz Timings</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>Name:</td>
+            <td>{Name}</td>
+          </tr>
+          <tr>
+            <td>Fajr:</td>
+            <td>{Fajr}</td>
+          </tr>
+          <tr>
+            <td>Zohar:</td>
+            <td>{Zohar}</td>
+          </tr>
+          <tr>
+            <td>Asar:</td>
+            <td>{Asar}</td>
+          </tr>
+          <tr>
+            <td>Magrib:</td>
+            <td>{Magrib}</td>
+          </tr>
+          <tr>
+            <td>Isha:</td>
+            <td>{Isha}</td>
+          </tr>
+          <tr>
+            <td>Juma:</td>
+            <td>{Juma}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
